62912477e786c53358cef0e9ccf4de7aae404287
Celshade <ggcelshade@gmail.com>
2025-09-18 02:11:28 -0400

    remap Profile.__init__(); remove test formatting for __repr__ & __str__; implement data property

--- src/profiles.py
+++ src/profiles.py
@@ -1,20 +1,82 @@
+from os import rename
+import json
+
+
 class Profile():
     """
     """
-    def __init__(self, data: dict[str, str]):
-        self._data = data
+    def __init__(self, name: str, frequency: int, coreVoltage: int):
+        self._name = name
+        self._frequency = frequency
+        self._coreVoltage = coreVoltage
 
     def __repr__(self):
-        return f"__repr__ ->>\n Profile({self._data})"  # TODO remove __repr__ text
+        return f"Profile({self._name}, {self._frequency}, {self._coreVoltage})"
 
     def __str__(self):
-        return "This is a profile printed from str()\n"
+        return json.dumps(self.data, indent=4)
 
-    def update_profile():
+    @property
+    def data(self):
+        return {
+            "name": self._name,
+            "frequency": self._frequency,
+            "coreVoltage": self._coreVoltage
+        }
+
+    def validate_profile(self) -> bool:
+        return 
+
+    @data.setter
+    def data(self, new_data: dict):
+        if "name" in new_data and isinstance(new_data["name"], str):
+            self._name == new_data["name"]
+            print(f"Upda {self._name}")
+        
+        if "frequency" in new_data and isinstance(new_data["frequency"], str):
+            self._name == new_data["frequency"]
+        
+        if "coreVoltage" in new_data and isinstance(new_data["coreVoltage"], str):
+            self._name == new_data["coreVoltage"]
+
+
+
+        if "name" in new_data:
+            self._name = new_data["name"]
+        else:
+            print("no name")
+        # self._name = new_data.get("name") or self._name
+        # self._frequency = new_data.get("frequency") or self._frequency
+        # self._coreVoltage = new_data.get("coreVoltage") or self._coreVoltage
+
+        # if name:
+        #     self._data["name"] = name
+        # if frequency:
+        #     self._data["frequency"] = frequency
+        # if coreVoltage:
+        #     self._data["coreVoltage"] = coreVoltage
+
+
+    def update_profile(
+            self,
+            name: str = None,
+            frequency: str = None,
+            coreVoltage: str = None
+        ) -> None:
+        """
+        """
+        return NotImplementedError
+
+    # def save_profile(self):
+    #     try:
+    #         assert all(self._data[i] for i in self._data)
+    #         with open(f"{self._data['name']}", 'w'):
+
+    #     except AssertionError:
+    #         print("No profile name set - cannot save profile.")
+
+    def run_profile(self):
         raise NotImplementedError
 
-    def run_profile():
-        raise NotImplementedError
-
-    def delete_profile():
+    def delete_profile(self):
         raise NotImplementedError
