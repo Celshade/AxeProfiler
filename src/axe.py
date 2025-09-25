b67f1c816e301399d7cd26fba20515a93c88ddba
Celshade <ggcelshade@gmail.com>
2025-09-25 16:01:57 -0400

    Add GPLv3 and copyright notices

--- src/axe.py
+++ src/axe.py
@@ -1,3 +1,22 @@
+# AxeProfiler is a program designed to make saving/switching configurations for
+# bitcoin miner devices simpler and more efficient.
+
+# Copyright (C) 2025 [DC] Celshade <ggcelshade@gmail.com>
+
+# This file is part of AxeProfiler.
+
+# AxeProfiler is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# AxeProfiler is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# AxeProfiler. If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import os
 import time  # TODO remove after testing
