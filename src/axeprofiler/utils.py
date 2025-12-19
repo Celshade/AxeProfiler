a0769efc109ce6db2a51678c3ee5a81b6da35dbf
Celshade <ggcelshade@gmail.com>
2025-12-19 16:55:51 -0500

    Breakout validate_config() from Cli.__init__()

--- src/axeprofiler/utils.py
+++ src/axeprofiler/utils.py
@@ -2,11 +2,19 @@ import json
 from os import mkdir, path
 
 from rich import print as rprint
-from rich.console import Console
 
 
-def validate_config():
-    pass
+def validate_config(config_path: str, root_path: str) -> dict[str, str]:
+    # Check for existing config or create one
+    if not path.exists(config_path):
+        with open(config_path, 'w') as f:
+            config = {"profile_dir": f"{root_path}.profiles/"}
+            f.write(json.dumps(config, indent=4))
+    else:  # Read existing config
+        with open(config_path, 'r') as f:
+            config = json.loads(f.read())
+
+    return config
 
 
 def validate_profile_dir(config: dict, config_file: str,
