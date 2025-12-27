6dfb6c7f1567114c121e0078bd63e393f31f102d
Celshade <ggcelshade@gmail.com>
2025-12-27 18:18:39 -0500

    Issue #33: Create mock input fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,61 @@
+import unittest.mock
+from io import StringIO
+
+import pytest
+
+
+@pytest.fixture
+def mock_q():
+    """Mock and yield a 'Q' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_exclaim():
+    """Mock and yield a '!!' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_p():
+    """Mock and yield a 'P' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_c():
+    """Mock and yield a 'C' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_m():
+    """Mock and yield a 'M' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_n():
+    """Mock and yield a 'N' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_d():
+    """Mock and yield a 'D' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
+
+@pytest.fixture
+def mock_r():
+    """Mock and yield a 'R' input entry for AxeProfiler."""
+    with unittest.mock.patch("sys.stdin", StringIO('Q')):
+        yield
+
