be50995a541d10eececadba9d164c7fa8ace9017
Celshade <ggcelshade@gmail.com>
2025-12-27 20:06:06 -0500

    Issue #33: Test for temp profile dir

--- tests/test_list.py
+++ tests/test_list.py
@@ -8,11 +8,14 @@ from axeprofiler.cli import Cli
 # NOTE `tmp_path` is a pytest built-in that creates a per-test tmp dir
 #   breaks in a class for some reason - returns a list instead of str path
 def test_no_profiles(tmp_path, mock_q, capsys):
-    with TempConfig(str(tmp_path.joinpath())) as temp:
+    path = str(tmp_path.joinpath())
+    with TempConfig(path):
+        # TODO break these config file asserts into their own test
         assert os.path.exists(".config")
         assert os.path.exists(".config.backup")
         with open(".config", 'r') as f:
-            print(json.loads(f.read()), end='\n')
+            assert json.loads(f.read()).get("profile_dir") == path
+            # print(json.loads(f.read()), end='\n')
 
         cli = Cli()
         assert cli.num_profiles == 0
