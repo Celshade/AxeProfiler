7b1c78da816d6bac5a6fbc6c110c5ce8a80f6181
Celshade <ggcelshade@gmail.com>
2025-12-27 23:44:26 -0500

    Issue #35: Implement test_list_one_page()

--- tests/test_list.py
+++ tests/test_list.py
@@ -8,7 +8,7 @@ from axeprofiler.cli import Cli
 # NOTE `tmp_path` is a pytest built-in that creates a per-test tmp dir
 #   breaks in a class for some reason - returns a list instead of str path
 def test_no_profiles(tmp_path, mock_q, capsys):
-    path = str(tmp_path.joinpath())
+    path = str(tmp_path.joinpath()) + '/'
     with TempConfig(path):
         # TODO break these config file asserts into their own test
         assert os.path.exists(".config")
@@ -17,16 +17,32 @@ def test_no_profiles(tmp_path, mock_q, capsys):
             assert json.loads(f.read()).get("profile_dir") == path
             # print(json.loads(f.read()), end='\n')
 
+        # Test for 0 profiles
         cli = Cli()
         assert cli.num_profiles == 0
         cli.list_profiles(first_page=True)
 
+        # Test for no [P] (page) option
         out = str(capsys.readouterr())
         assert "Enter [P]" not in out
 
 
-# def test_list_one_page():
-#     raise NotImplementedError
+def test_list_one_page(tmp_path, mock_q, capsys):
+    path = str(tmp_path.joinpath()) + '/'
+    # print(path)  # NOTE testing
+    with TempConfig(path, 1):
+        # print(os.listdir(path))  # NOTE testing
+        # Test for 1 profiles
+        cli = Cli()
+        assert cli.profile_dir == path
+        assert cli.num_profiles == 1
+        cli.list_profiles(first_page=True)
+
+        # Test for no [P] (page) option
+        out = str(capsys.readouterr())
+        assert "Enter [P]" not in out
+        # assert 0  # NOTE testing
+
 
 
 # def test_list_mul_page():
