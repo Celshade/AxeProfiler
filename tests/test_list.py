8610a85ae12dcdf8473e07663a6996a60ce0f84b
Celshade <ggcelshade@gmail.com>
2026-01-18 17:05:40 -0500

    Make create_cli_and_test_list() to handle all list setup and output validation. Test functions now parameterize as-needed and call this main function to test against. Update TODO/FIXME. test_list_mul_page_7() failing.

--- tests/test_list.py
+++ tests/test_list.py
@@ -5,11 +5,9 @@ from shared import TempConfig
 from axeprofiler.cli import Cli
 
 
-# TODO breakout core test logic into a function with strong param flexibility
-#  set args and call from within each test_func() to consolidate
+# NOTE `tmp_path` == pytest Fixture that creates a per-test temp dir
 def test_config_handling(tmp_path: str):
-    path = str(tmp_path.joinpath()) + '/'
-
+    path = f"{tmp_path.joinpath()}/"
     with TempConfig(path):
         # Validate config setup and backup
         assert os.path.exists(".config")
@@ -25,110 +23,92 @@ def test_config_handling(tmp_path: str):
     assert not os.path.exists(".config.backup")
 
 
-# NOTE `tmp_path` is a pytest built-in that creates a per-test tmp dir
-#   breaks in a class for some reason - returns a list instead of str path
-def test_no_profiles(tmp_path, mock_q, capsys):
-    path = str(tmp_path.joinpath()) + '/'
-    with TempConfig(path):
-        # Test for 0 profiles
-        cli = Cli()
-        assert cli.num_profiles == 0
-        cli.list_profiles(first_page=True)
+def create_cli_and_test_list(
+        path: str,
+        stdout: str | None = None,
+        num_profiles: int | None = 0,
+        first_page: bool = True,
+        profiles: list[dict] = None,  # TODO edit type
+        num_rendered: int = 0,
+        message: str | None = None,  # TODO message_in: list[str]; check all;
+        # TODO message_out: list[str]; check all
+        message_in: bool = False
+):
+    with TempConfig(path, num_profiles=num_profiles):
+        cli = Cli()  # init CLI
+        assert cli.profile_dir == path  # Validate path
+        assert cli.num_profiles == num_profiles  # Validate profile count
 
-        # Test for no [P] (page) option
-        out = str(capsys.readouterr())
-        assert "[P]" not in out
+        # Handle page listing
+        cli.list_profiles(profiles=profiles, num_rendered=num_rendered,
+                          first_page=first_page)
+
+        # Validate output
+        if message and stdout:
+            output = str(stdout.readouterr())
+
+            if message_in:
+                print(f"\n{output}")
+                assert message in output
+            else:
+                print(f"\n{output}")
+                assert message not in output
+
+
+def test_no_profiles(tmp_path, mock_q, capsys):
+    path = f"{tmp_path.joinpath()}/"
+    create_cli_and_test_list(path=path, stdout=capsys,
+                             num_profiles=0, message="[P]")
 
 
 def test_list_one_page(tmp_path, mock_q, capsys):
-    path = str(tmp_path.joinpath()) + '/'
-    # print(path)  # NOTE testing
-    with TempConfig(path, 1):
-        # print(os.listdir(path))  # NOTE testing
-        # Test for 1 profiles
-        cli = Cli()
-        assert cli.profile_dir == path
-        assert cli.num_profiles == 1
-        cli.list_profiles(first_page=True)
-
-        # Test for no [P] (page) option
-        out = str(capsys.readouterr())
-        assert "[P]" not in out
-        # assert 0  # NOTE testing
+    path = f"{tmp_path.joinpath()}/"
+    create_cli_and_test_list(path=path, stdout=capsys,
+                             num_profiles=1, message="[P]")
 
 
 def test_list_mul_page_q(tmp_path, mock_q, capsys):
-    path = str(tmp_path.joinpath()) + '/'
-    # print(path)  # NOTE testing
-    with TempConfig(path, 6):
-        # print(os.listdir(path))  # NOTE testing
-        # Test for more than 1 page (4 per page)
-        cli = Cli()
-        assert cli.profile_dir == path
-        assert cli.num_profiles == 6
-        cli.list_profiles(first_page=True)
-
-        # Test for [P] (page) option
-        out = str(capsys.readouterr())
-        # print(out)  # NOTE testing
-        assert "[P]" in out
-        # assert 0  # NOTE testing
+    path = f"{tmp_path.joinpath()}/"
+    create_cli_and_test_list(path=path, stdout=capsys,
+                             num_profiles=6, message="[P]", message_in=True)
 
 
+# FIXME x-y count is off on the upper end (tests-only)
+# FIXME [P] showing in output where it shouldnt
 def test_list_mul_page_7(tmp_path, mock_7, capsys):
-    path = str(tmp_path.joinpath()) + '/'
-    # print(path)  # NOTE testing
-    with TempConfig(path, 7):
-        # print(os.listdir(path))  # NOTE testing
-        # Test for more than 1 page (4 per page)
-        cli = Cli()
-        assert cli.profile_dir == path
-        assert cli.num_profiles == 7
-        cli.list_profiles(profiles=os.listdir(cli.profile_dir)[4:],
-                          num_rendered=4)
-
-        # Test for [P] (page) option removed due to prompt clear on final page
-        out = str(capsys.readouterr())
-        print(out)  # NOTE testing
-        assert "[P]" not in out
-        # assert 0  # NOTE testing
+    path = f"{tmp_path.joinpath()}/"
+    create_cli_and_test_list(path=path, stdout=capsys,
+                             num_profiles=7, first_page=False,
+                             profiles=os.listdir(path)[4:],
+                             num_rendered=4, message="[P]")
 
 
 def test_list_mul_page_select_forward(tmp_path, mock_7, capsys):
-    path = str(tmp_path.joinpath()) + '/'
-    # print(path)  # NOTE testing
-    with TempConfig(path, 7):
-        # print(os.listdir(path))  # NOTE testing
-        # Test for more than 1 page (4 per page)
-        cli = Cli()
-        assert cli.profile_dir == path
-        assert cli.num_profiles == 7
-        cli.list_profiles(first_page=True)
+    path = f"{tmp_path.joinpath()}/"
+    create_cli_and_test_list(path=path, num_profiles=7)
 
-        # Test for [P] (page) option and 7 in choices (which is mocked input)
-        out = str(capsys.readouterr())
-        # print(out)  # NOTE testing
-        assert all(("[P]" in out, '7' in out))
-        # assert 0  # NOTE testing
+    # Test for [P] (page) option and 7 in choices (which is mocked input)
+    out = str(capsys.readouterr())
+    assert all(("[P]" in out, '7' in out))
 
 
-def test_list_mul_page_select_retro(tmp_path, mock_1, capsys):
-    path = str(tmp_path.joinpath()) + '/'
-    # print(path)  # NOTE testing
-    with TempConfig(path, 5):
-        # print(os.listdir(path))  # NOTE testing
-        # Test for more than 1 page (4 per page)
-        cli = Cli()
-        assert cli.profile_dir == path
-        assert cli.num_profiles == 5
-        cli.list_profiles(profiles=os.listdir(cli.profile_dir)[4:],
-                          num_rendered=4)
+# def test_list_mul_page_select_retro(tmp_path, mock_1, capsys):
+#     path = f"{tmp_path.joinpath()}/"
+#     # print(path)  # NOTE testing
+#     with TempConfig(path, 5):
+#         # print(os.listdir(path))  # NOTE testing
+#         # Test for more than 1 page (4 per page)
+#         cli = Cli()
+#         assert cli.profile_dir == path
+#         assert cli.num_profiles == 5
+#         cli.list_profiles(profiles=os.listdir(cli.profile_dir)[4:],
+#                           num_rendered=4)
 
-        # Test successful pagination and retro selection
-        out = str(capsys.readouterr())
-        print(out)  # NOTE testing
-        assert "[P]" not in out
+#         # Test successful pagination and retro selection
+#         out = str(capsys.readouterr())
+#         print(out)  # NOTE testing
+#         assert "[P]" not in out
 
-        assert cli.profile
-        assert cli.profile.name == os.listdir(cli.profile_dir)[0].split('.')[0]
-        # assert 0  # NOTE testing
+#         assert cli.profile
+#         assert cli.profile.name == os.listdir(cli.profile_dir)[0].split('.')[0]
+#         # assert 0  # NOTE testing
